48f891e38164cae7ec2d3e49a6742654485b3f66
拉格朗日无悔 <2545197103@qq.com>
2019-02-24 20:10:36 +0800

    PolyNumber

---
+++ Poly/src/com/test/poly/B.java
@@ -0,0 +1,8 @@
+package com.test.poly;
+
+public class B extends A{
+	public int a=1;
+	public void fun(){
+		System.out.println("B");
+	}
+}
---
+++ Poly/src/com/test/poly/Main.java
@@ -0,0 +1,9 @@
+package com.test.poly;
+
+public class Main {
+	public static void main(String[] args){
+		A classA = new B();
+		System.out.println(classA.a);
+		classA.fun();
+	}
+}
