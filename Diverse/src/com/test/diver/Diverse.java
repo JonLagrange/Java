a8394f39274f0f3c084eb4159d709d0383dffc77
拉格朗日无悔 <2545197103@qq.com>
2019-02-24 20:00:30 +0800

    字符串反转

---
+++ Diverse/src/com/test/diver/Diverse.java
@@ -0,0 +1,10 @@
+package com.test.diver;
+import java.util.Scanner;
+
+public class Diverse {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		StringBuffer sb =new StringBuffer(in.nextLine());
+	    System.out.println(sb.reverse());
+	}
+}
